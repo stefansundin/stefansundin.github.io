c7dd5f8e577ecd18ca3d83d96eec26371c9e890a
Stefan Sundin <git@stefansundin.com>
2015-08-15 16:15:44 -0700

    https whitelist .github.io

--- js/https.js
+++ js/https.js
@@ -1,3 +1,3 @@
-if (window.location.protocol == "http:" && !/\.dev$/.test(window.location.hostname)) {
+if (window.location.protocol == "http:" && /\.github\.io$/.test(window.location.hostname)) {
   window.location = "https:" + window.location.href.substring(5);
 }
