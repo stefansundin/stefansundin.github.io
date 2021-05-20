e34062fe83d5e2523ead5d5c61e54fdb06c5c24d
Stefan Sundin <git@stefansundin.com>
2021-05-19 18:26:01 -0700

    JavaScript updates.

--- js/main.js
+++ js/main.js
@@ -1,27 +1,36 @@
-var links = document.querySelectorAll(".expander");
-for (var i=0; i < links.length; i++) {
-  var a = links[i];
+document.querySelectorAll(".javascript").forEach(function(el) {
+  el.classList.remove("javascript");
+});
+
+document.querySelectorAll(".expander").forEach(function(a) {
   a.style.display = "inline-block";
   a.addEventListener("click", function() {
-    var id = this.getAttribute("expand");
+    const id = this.getAttribute("expand");
     document.getElementById(id).style.display = "block";
     this.style.display = "none";
   });
-  var id = a.getAttribute("expand");
+  const id = a.getAttribute("expand");
   document.getElementById(id).style.display = "none";
-}
+});
 
-var forms = document.querySelectorAll('form[action="https://www.paypal.com/cgi-bin/webscr"]');
-for (var i=0; i < forms.length; i++) {
-  var form = forms[i];
-  form.addEventListener('submit', function(e) {
+document.querySelectorAll('form[action="https://www.paypal.com/cgi-bin/webscr"]').forEach(function(form) {
+  form.addEventListener("submit", function(e) {
     if (parseInt(this.amount.value) < 1) {
-      if (ga) {
-        ga('send', 'event', 'paypal', 'click', `paypal cheapskate ${this.amount.value}`);
-      }
       e.preventDefault();
       alert("The minimum donation amount is one dollar. Anything less than one dollar and you're just giving PayPal everything because of their fees.\n\nIf you can't afford one dollar, then please donate to a local charity instead.");
       return false;
     }
   });
-}
+});
+
+document.querySelectorAll("a[fubar]").forEach(function(a) {
+  if (a.href != "") {
+    return;
+  }
+  a.textContent = a.textContent.replace(/[A-Z]{2}/, function(c) {
+    return (c[0] + "@" + c[1]).toLowerCase();
+  }).replace(/[A-Z]/g, function(c) {
+    return "." + c.toLowerCase();
+  });
+  a.href = "mailto:" + a.textContent;
+});
